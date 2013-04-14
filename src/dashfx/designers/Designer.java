c439b402942479646c86e1f405bc7ed3d9bfcf06
Patrick Plenefisch <simonpatp@gmail.com>
2013-04-14 15:48:40 -0400

    Adding designers

---
+++ src/dashfx/designers/Designer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dashfx.designers;
+
+/**
+ *
+ * @author patrick
+ */
+public @interface Designer
+{
+	Class value();
+}
