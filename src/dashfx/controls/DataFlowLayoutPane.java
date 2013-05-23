0683c23adeb4a3a4799eeebe44ff96be48838eb0
Patrick Plenefisch <simonpatp@gmail.com>
2013-05-23 15:15:28 -0400

    Adding FlowPane & base pane class

---
+++ src/dashfx/controls/DataFlowLayoutPane.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2013 patrick
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dashfx.controls;
+
+import dashfx.data.ZPositions;
+import java.util.Arrays;
+import java.util.EnumSet;
+import javafx.scene.Node;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Region;
+
+/**
+ *
+ * @author patrick
+ */
+@Designable(value = "Flow", description = "Automatic flowing pane")
+@DesignableProperty(value = {"hgap", "vgap"}, descriptions = {"Horizontal Spacing", "Vertical spacing"})
+public class DataFlowLayoutPane extends PaneControlBase<FlowPane>
+{
+	public DataFlowLayoutPane()
+	{
+		super(new FlowPane());
+	}
+
+	@Override
+	public EnumSet<ResizeDirections> getSupportedOps()
+	{
+		return EnumSet.copyOf(Arrays.asList(ResizeDirections.values()));
+	}
+
+	@Override
+	public boolean isJumps()
+	{
+		return true;
+	}
+
+	@Override
+	public void BeginDragging(Node[] overlays, Region[] childs, double x, double y, double sizeX, double sizeY, double posX, double posY)
+	{
+		// TODO: FIXME
+	}
+
+	@Override
+	public void ContinueDragging(double dx, double dy)
+	{
+		// TODO: FIXME
+	}
+
+	@Override
+	public void FinishDragging()
+	{
+		// TODO: FIXME
+	}
+
+	@Override
+	public void addChildAt(Node child, double x, double y)
+	{
+		ui.getChildren().add(child);
+		//TODO: Fix
+	}
+
+	@Override
+	public void editNested(Node overlay, Runnable onExitRequest)
+	{
+		// TODO: FIXME
+	}
+
+	@Override
+	public void exitNested()
+	{
+		// TODO: FIXME
+	}
+
+	@Override
+	public void zEdit(Node child, ZPositions diff)
+	{
+		// dnc
+	}
+}
---
+++ src/dashfx/controls/PaneControlBase.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2013 patrick
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dashfx.controls;
+
+import dashfx.data.*;
+import java.util.*;
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+
+/**
+ *
+ * @author patrick
+ */
+public abstract class PaneControlBase<T extends Pane> implements DataCoreProvider, Control, DesignablePane
+{
+	private DataCoreProvider superprovider = null;
+	private ArrayList<Registerable> unregistered = new ArrayList<>();
+	private ArrayList<Registerable> registered = new ArrayList<>();
+	private boolean designing;
+	private boolean nested = false;
+
+	public PaneControlBase(T pane)
+	{
+		ui = pane;
+		ui.getChildren().addListener(new ListChangeListener<Node>()
+		{
+			@Override
+			@SuppressWarnings("element-type-mismatch")
+			public void onChanged(ListChangeListener.Change<? extends Node> change)
+			{
+				change.next();
+				for (Node n : change.getAddedSubList())
+				{
+					if (n instanceof Registerable && !unregistered.contains(n) && !registered.contains(n))
+						addControl((Registerable) n);
+				}
+			}
+		});
+	}
+
+	@Override
+	public void addControl(Registerable r)
+	{
+		if (superprovider != null)
+		{
+			r.registered(this);
+			registered.add(r);
+		}
+		else
+			unregistered.add(r);
+	}
+
+	@Override
+	public void addDataEndpoint(DataEndpoint r)
+	{
+		superprovider.addDataEndpoint(r);
+	}
+
+	@Override
+	public void addDataFilter(DataProcessor r)
+	{
+		superprovider.addDataFilter(r);
+	}
+
+	@Override
+	public void clearAllDataEndpoints()
+	{
+		superprovider.clearAllDataEndpoints();
+	}
+
+	@Override
+	public void mountDataEndpoint(DataInitDescriptor<DataEndpoint> r)
+	{
+		superprovider.mountDataEndpoint(r);
+	}
+
+	@Override
+	public DataInitDescriptor<DataEndpoint>[] getAllDataEndpoints()
+	{
+		return superprovider.getAllDataEndpoints();
+	}
+
+	@Override
+	public DataProcessor[] getAllDataFilters()
+	{
+		return superprovider.getAllDataFilters();
+	}
+
+	@Override
+	public SmartValue getObservable(String name)
+	{
+		return superprovider.getObservable(name);
+	}
+
+	@Override
+	public void registered(DataCoreProvider provider)
+	{
+		superprovider = provider;
+		for (Registerable r : unregistered)
+		{
+			r.registered(this);
+			registered.add(r);
+		}
+	}
+
+	@Override
+	public void pause()
+	{
+		superprovider.pause();
+	}
+
+	@Override
+	public void resume()
+	{
+		superprovider.resume();
+	}
+
+	@Override
+	public void setDesigning(boolean designing)
+	{
+		this.designing = designing;
+	}
+
+	@Override
+	public boolean getDesigning()
+	{
+		return designing;
+	}
+
+	@Override
+	public void dispose()
+	{
+		superprovider.dispose();
+	}
+	T ui;
+
+	@Override
+	public Node getUi()
+	{
+		return ui;
+	}
+}
