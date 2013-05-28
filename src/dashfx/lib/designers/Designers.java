72852fe61570056aa867a61b5c170d0f1e2a7675
Patrick Plenefisch <simonpatp@gmail.com>
2013-05-28 16:58:48 -0400

    removing hackiness

--- src/dashfx/lib/designers/Designers.java
+++ src/dashfx/lib/designers/Designers.java
@@ -24,7 +24,13 @@ public class Designers
 		all.put(Integer.class, NumberDesigner.class);
 		all.put(Float.class, NumberDesigner.class);
 		all.put(Number.class, NumberDesigner.class);
+		all.put(double.class, NumberDesigner.class);
+		all.put(int.class, NumberDesigner.class);
+		all.put(float.class, NumberDesigner.class);
+		all.put(long.class, NumberDesigner.class);
+		all.put(short.class, NumberDesigner.class);
 		all.put(Boolean.class, BoolDesigner.class);
+		all.put(boolean.class, BoolDesigner.class);
 	}
 
 	public static void addDesigner(Class type, Class designer)
@@ -32,12 +38,12 @@ public class Designers
 		all.put(type, designer);
 	}
 
-	public static PropertyDesigner getDesignerForClass(Class type)
+	public static PropertyDesigner getFor(Class type)
 	{
 		//TODO: if not found, search entire classpath
 		Class dznrClz = all.get(type);
 		if (dznrClz == null)
-			return null; //TODO: should this throw stuff?
+			return null;
 		try
 		{
 			return (PropertyDesigner) dznrClz.newInstance();
@@ -46,26 +52,5 @@ public class Designers
 		{
 			return null;
 		}
-	}
-
-	public static PropertyDesigner getDesignerFor(Property p)
-	{
-		Class it = null;
-		//FIXME: SO HACK!
-		if (p instanceof DoubleProperty)
-		{
-			it = Double.class;
-		}
-		else if (p instanceof StringProperty)
-		{
-			it = String.class;
-		}
-		else if (p instanceof BooleanProperty)
-		{
-			it = Boolean.class;
-		}
-		PropertyDesigner pd = getDesignerForClass(it);
-		pd.design(p);
-		return pd;
 	}
 }
